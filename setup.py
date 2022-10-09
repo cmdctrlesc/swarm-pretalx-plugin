c0ebdf723ca615f6e33b8d51e98bd1a2345557d4
cmdctrlesc <alanxyz@gmail.com>
2022-10-09 23:40:00 +0200

    bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ cmdclass = {"build": CustomBuild}
 
 setup(
     name="swarm-plugin",
-    version="0.1.3",
+    version="0.1.4",
     description="Pretalx plugin for exporting the agenda to Swarm",
     long_description=long_description,
     url="https://github.com/cmdctrlesc/swarm-pretalx-plugin",
